daa7f8f2fa257d159a8aaff3ab9e971da1ee4611
Ruixuan1225 <100375491+Ruixuan1225@users.noreply.github.com>
2022-12-05 13:42:16 -0500

    Delete cardio_training.R

--- cardio_training.R
+++
@@ -1,9 +0,0 @@
-library(readxl);
-library(tidyverse);
-
-data <- read.csv("source_data/cardio_train.csv", header=TRUE, sep=";");
-
-#scale the data
-scaled <- as.data.frame(scale(data[,2:12], center=TRUE,scale= TRUE));
-
-write.csv(scaled, "output/scaled_data.csv");
